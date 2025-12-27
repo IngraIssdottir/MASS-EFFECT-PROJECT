70e9b105ffc128992b57a820cc194516b65ab2c9
Ingra Issdottir <ingra.issdottir@yahoo.es>
2025-12-27 11:59:58 +0100

    connecting backend with mysql data base

--- BACK-END/src/index.js
+++ BACK-END/src/index.js
@@ -5,10 +5,10 @@ const express = require('express');
 const cors = require('cors');
 
 // Impportar biblioteca de mysql2
-/*const mysql = require("mysql2/promise");
+const mysql = require("mysql2/promise");
 const path = require('node:path');
 
-require('dotenv').config();*/
+require('dotenv').config();
 
 // Crear una variable con todo lo que puede hacer el servidor:
 const server = express();
@@ -23,52 +23,26 @@ server.listen(port, () => {
   console.log(`Uh! El servidor ya está arrancado: <http://localhost:${port}/>`);
 });
 
-const characters = [
-  {
-    characterId: 0,
-    name: "Commander Shepard",
-    gender: "Female/Male",
-    race: "Human",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmJw7yivIx9SU6dVshPN1L8h-J-tGcZNGvNw&s",
-  },
-  {
-    characterId: 1,
-    name: "Garrus Vakarian",
-    gender: "Male",
-    race: "Turian",
-    img: "https://static.wikia.nocookie.net/masseffect/images/3/36/Garrus_Character_Shot.png",
-  },
-  {
-    characterId: 2,
-    name: "Tali'Zorah nar Rayya",
-    gender: "Female",
-    race: "Quarian",
-    img: "https://static.wikia.nocookie.net/masseffect/images/2/2f/Talicharactershotme2.jpg",
-  },
-  {
-    characterId: 3,
-    name: "Legion",
-    gender: "A.I.",
-    race: "Geth",
-    img: "https://static.wikia.nocookie.net/masseffect/images/2/25/Legion.png",
-  },
-];
 
-
-/*async function getConection() {
-    host: 'localhost'
-    port: 3306
-    database: 'me-codex'
-    user: 'root'
-    password: process.env.MYSQL_PASSWORD
-};*/
+async function getConnection() {
+  const  connection = await mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    database: 'me-codex',
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+  });
+    return connection;
+}
 
 server.get('/', (req, res) => {
-    req.send('Ok!');
+    res.send('Ok!');
 });
 
-server.get('/api/characters', (req, res) =>{
-    res.json(characters);
-
-    
-})
+server.get('/api/characters', async (req, res) =>{
+    const connection = await getConnection();
+    const [rows] = await connection.query(
+      `SELECT * FROM characters`
+    );
+    res.json(rows);
+});
