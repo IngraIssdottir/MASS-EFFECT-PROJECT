4a8ec8da5c3c6be182e19f70f99802f406d20859
Ingra Issdottir <ingra.issdottir@yahoo.es>
2025-12-27 13:44:54 +0100

    fixing images from species

--- FRONT-END/src/pages/Planets.jsx
+++ FRONT-END/src/pages/Planets.jsx
@@ -1,6 +1,53 @@
-function Planets() {
-return <h1>Planetas</h1>;
-}
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router";
 
+function Planets() {
+  //const [charactersList, setCharactersList] = useState(characters);
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/planets")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPlanets(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <section className="list_title_cont">
+        <h1 className="list_title">Planets</h1>
+        <NavLink className="link" to="/">
+          Volver a Inicio
+        </NavLink>
+      </section>
+
+      <section className="cards_cont">
+        {planets.map((planet) => {
+          return (
+            <article className="item_card" key={planet.planetId}>
+              <div className="image_cont">
+                <img
+                  className="item_img"
+                  src={planet.picture}
+                  alt={`imagen de ${planet.name}`}
+                />
+              </div>
+              <div>
+                <h4>{planet.name}</h4>
+                <p>{planet.description}</p>
+                <p>{planet.home_race}</p>
+              </div>
+            </article>
+          );
+        })}
+        ;
+      </section>
+    </div>
+  );
+
+}
 
 export default Planets;
--- FRONT-END/src/pages/Races.jsx
+++ FRONT-END/src/pages/Races.jsx
@@ -1,6 +1,54 @@
-function Races() {
-return <h1>Especies</h1>;
-}
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router";
 
+function Races() {
+  //const [charactersList, setCharactersList] = useState(characters);
+  const [races, setRaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/races")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setRaces(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <section className="list_title_cont">
+        <h1 className="list_title">Species</h1>
+        <NavLink className="link" to="/">
+          Volver a Inicio
+        </NavLink>
+      </section>
+
+      <section className="cards_cont">
+        {races.map((race) => {
+          return (
+            <article className="item_card" key={race.raceId}>
+              <div className="image_cont">
+                <img
+                  className="item_img"
+                  src={race.picture}
+                  alt={`imagen de ${race.name}`}
+                />
+              </div>
+              <div>
+                <h4>{race.name}</h4>
+                <p>Description: {race.description}</p>
+                <p>Home wolrd: {race.home_world}</p>
+                <p>Category: {race.category}</p>
+              </div>
+            </article>
+          );
+        })}
+        ;
+      </section>
+    </div>
+  );
+
+}
 
 export default Races;
